64922304b1628e6a430e8dcdd3c0f77a8df1a580
23061983 <43507935+23061983@users.noreply.github.com>
2025-05-24 23:13:56 -0300

    Atualizar para sites.json

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-[
+
     {"name": "Twitter", "url": "https://twitter.com/{username}"},
     {"name": "GitHub", "url": "https://github.com/{username}"},
     {"name": "Instagram", "url": "https://www.instagram.com/{username}"},
     {"name": "LinkedIn", "url": "https://www.linkedin.com/in/{username}"},
     {"name": "Reddit", "url": "https://www.reddit.com/user/{username}"}
-]
+
